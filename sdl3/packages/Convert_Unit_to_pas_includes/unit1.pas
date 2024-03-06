daad44f8d156f9c3c8725b32894a6aa81020be40
sechshelme <you@example.com>
2024-03-06 18:10:13 +0100

    sdl3

--- sdl3/packages/Convert_Unit_to_pas_includes/unit1.pas
+++ sdl3/packages/Convert_Unit_to_pas_includes/unit1.pas
@@ -5,6 +5,7 @@ unit Unit1;
 interface
 
 uses
+  gtk2,
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, FileUtil;
 
 type
@@ -33,7 +34,7 @@ implementation
 
 procedure TForm1.Button1Click(Sender: TObject);
 var
-  slFile, unit_source, inc_dest: TStringList;
+  slFile, unit_source, inc_dest, SDL3pas_include: TStringList;
   i, j: integer;
   path: string;
 begin
@@ -45,14 +46,6 @@ begin
     inc_dest := TStringList.Create;
     unit_source.LoadFromFile(slFile[i]);
 
-    path := ExtractFileName(slFile[i]);
-    path := ChangeFileExt(path, '.inc');
-
-    path:='/n4800/DATEN/Programmierung/mit_GIT/Lazarus/Tutorial/SDL-2/sdl3/packages/pas_includes/'+path;
-
-    Memo1.Lines.Add(path);
-
-
     j := 0;
     repeat
       inc_dest.Add(unit_source[j]);
@@ -60,16 +53,30 @@ begin
 
       Inc(j)
     until j >= unit_source.Count;
-        inc_dest.SaveToFile(path);
 
-    for j := 0 to unit_source.Count - 1 do begin
-      //      unit_source[j] := StringReplace(unit_source[j], 'SDLCALL', '', [rfReplaceAll]);
-      //    unit_source[j] := StringReplace(unit_source[j], 'DECLSPEC', '', [rfReplaceAll]);
-    end;
-    unit_source.SaveToFile(slFile[i]);
+    path := ExtractFileName(slFile[i]);
+    path := ChangeFileExt(path, '.inc');
+    path := '/n4800/DATEN/Programmierung/mit_GIT/Lazarus/Tutorial/SDL-2/sdl3/packages/pas_includes/' + path;
+    Memo1.Lines.Add(path);
+
+    inc_dest.SaveToFile(path);
+
     inc_dest.Free;
     unit_source.Free;
   end;
+
+  // === SDL3_includes.inc
+
+  SDL3pas_include := TStringList.Create;
+  SDL3pas_include.Add('{%MainUnit sdl3.pas}');
+
+  for i := 0 to slFile.Count - 1 do begin
+    path := ExtractFileName(slFile[i]);
+    SDL3pas_include.Add('{$include ' + path + '.inc}');
+  end;
+
+  SDL3pas_include.SaveToFile('/n4800/DATEN/Programmierung/mit_GIT/Lazarus/Tutorial/SDL-2/sdl3/packages/pas_includes/SDL3_includes.inc');
+  SDL3pas_include.Free;
 
   slFile.Clear;
 end;
